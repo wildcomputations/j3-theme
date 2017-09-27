095b961e49cd99d2b6f5b9d83f35537ee9900833
Emilie Phillips <emilie@j3.org>
2017-09-26 22:35:11 -0400

    official style sheet bump

--- theme/functions.php
+++ theme/functions.php
@@ -70,7 +70,7 @@ function j3AddExternals() {
 
     $styleDir = get_stylesheet_directory_uri();
     wp_register_style( 'j3BaseStyle', $styleDir . '/style.css', 
-        array(), "2.35" );
+        array(), "3.0" );
     wp_enqueue_style('j3BaseStyle');
 
     wp_register_style( 'fontAwesome',
