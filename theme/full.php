e795b73c67072970473179aa1ed2637594103deb
Emilie Phillips <emilie@j3.org>
2020-09-05 11:34:40 -0400

    new mini map, and reordered side bar.
    Gallery doesn't yet have reordered side bar

--- theme/full.php
+++ theme/full.php
@@ -36,12 +36,27 @@ function j3ContentArticle() {
 function j3ContentComments()
 {
     if (comments_open()) {
-        echo '<article class="subPage">';
+        echo '<article class="subPage" id="commentsSection">';
         comments_template();
         echo '</article>';
     }
 }
 
+function j3SkipToComments()
+{
+    $num_comments = get_comments_number();
+    if (comments_open() || $num_comments > 0) {
+        echo '<div class="linkBlock">';
+        echo '<h1>Comments</h1>';
+        echo '<p><a href="#commentsSection">';
+        if ($num_comments > 0) {
+            echo 'Skip to comments (' . get_comments_number() . ')';
+        } else {
+            echo 'Leave a comment';
+        }
+        echo '</a></p></div><!--linkBlock-->';
+    }
+}
 
 function j3ArticleLinks() {
     $catHtml = j3AsideCategories();
@@ -64,6 +79,7 @@ function j3MapLinks() {
     if (! is_plugin_active("travelers-map/travelers-map.php") ) return;
     if (! metadata_exists(get_post_type(), get_the_ID(), '_latlngmarker') ) return;
     echo '<div class="linkBlock">';
+    echo '<h1>Nearby Trips</h1>';
     echo do_shortcode(
         '[travelers-map height=220px init_maxzoom=14 centered_on_this=true disable_clustering=true]');
     echo '</div>';
@@ -96,8 +112,9 @@ if ( post_password_required() ) {
 <?php
     if (get_post_type() == 'post' ) {
 ?>
-    <aside>
+    <aside class=leftBar>
         <?php 
+            j3SkipToComments();
             j3ArticleLinks();
             j3MapLinks();
             if(function_exists('echo_crp')) {
@@ -114,8 +131,6 @@ echo '</div><!--linkBlock-->';
     <aside class="mini">
         <a href="<?php the_permalink(); ?>">More details ...</a>
     </aside>
-</div> <!-- hgroup -->
-<div class="hgroup hasPage">
     <div class="rightContent">
 <?php j3ContentComments(); ?>
     </div> <!-- rightContent -->
